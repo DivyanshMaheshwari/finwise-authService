c33944488ec9956e2ce0739bde8e8f60d062f29b
divyansh <mah.divyansh@gmail.com>
2025-08-03 18:39:08 +0530

    added netlify url

--- src/main/java/com/finwise/authservice/config/CorsConfig.java
+++ src/main/java/com/finwise/authservice/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200",
+                                "https://finwise-app.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
